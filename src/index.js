94e010fba2ee57ae73f7539ff0faf8430ac25d4c
Animakkonu <lada.dunskaya@mail.ru>
2021-02-22 16:50:13 +0300

    faet: done min, max, avg function

--- src/index.js
+++ src/index.js
@@ -1,12 +1,25 @@
 
 exports.min = function min (array) {
-  return 0;
+  let min = 0;
+  if (Array.isArray(array) && array.length > 0){
+    for (let elem of array) if (elem < min) min = elem;
+    return min;
+  } else return 0;
+  
 }
 
 exports.max = function max (array) {
-  return 0;
+  let max = 0;
+  if (Array.isArray(array) && array.length > 0){
+    for (let elem of array) if (elem > max) max = elem;
+    return max;
+  } else return 0;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  let sum = 0;
+  if (Array.isArray(array) && array.length > 0){
+  for (let elem of array) sum+= elem;
+  return sum/array.length;
+  } else return 0;
 }
